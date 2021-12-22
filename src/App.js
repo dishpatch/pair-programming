fbb11cbe2c4a3e53c1590c33e9025bf423f41077
matthewiiv <matthewsharp123@gmail.com>
2021-12-22 08:07:41 +0000

    feat: initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useMediaQuery } from "./useMediaQuery";
+import { products } from "./api";
 
 function App() {
+  // Using the product data below, build 2 views demonstrating your
+  // ability to use React, Html and CSS. Be careful, not all the data has necessarily been
+  // entered correctly
+
+  // 1. A product page banner showing at least a photo of the dish and the title and description.
+  // People shop for food with their eyes so your design should be responsive.
+  // Feel free to use https://www.dishpatch.co.uk/feasts/mee-goreng as inspiration, this is not a design test.
+
+  // 2. standard ecommerce grid layout of product cards featuring and image, title and description
+  // along with any other information you want to include. Your grid should be responsive and should
+  // collapse down to one card per row on mobile, and 3 per row on desktop.
+  // Feel free to use https://www.dishpatch.co.uk/menu as inspiration.
+
+  const isMobile = useMediaQuery("(max-width: 480px)");
+
+  const productData = products;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main></main>
     </div>
   );
 }
---
+++ src/api.js
@@ -0,0 +1,69 @@
+export const products = [
+  {
+    title: "Express Roti",
+    handle: "express-roti",
+    description:
+      "Simply Roti King's most popular Malaysian curries, served with roti canai.",
+    vendor: "Roti King",
+    serves: 2,
+    imageUrl:
+      "https://www.dishpatch.co.uk/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0265%2F8847%2F9530%2Fproducts%2Fexpress-roti-feast-roti-king-986713.png%3Fv%3D1635239328&w=3840&q=75",
+    allergens: ["gluten", "nuts"],
+    dietaries: {
+      vegetarian: false,
+      vegan: false,
+      glutenFree: false,
+      dairyFree: true,
+    },
+  },
+  {
+    title: "Wild Mushroom and Artichoke Paella",
+    handle: "mushroom-artichoke-paella",
+    description: "José's hearty wild mushroom and artichoke paella with aoili.",
+    vendor: "José Pizarro",
+    serves: 2,
+    imageUrl:
+      "https://www.dishpatch.co.uk/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0265%2F8847%2F9530%2Fproducts%2Fwild-mushroom-artichoke-paella-feast-jose-pizarro-910090.jpg%3Fv%3D1637840327&w=3840&q=75",
+    allergens: ["gluten", "nuts"],
+    dietaries: {
+      vegetarian: true,
+      vegan: false,
+      glutenFree: false,
+      dairyFree: true,
+    },
+  },
+  {
+    title: "Golae Chicken",
+    handle: "golae-chicken",
+    description:
+      "A vibrant Thai feast featuring Southern-style grilled 'golae' chicken and soy braised beef cheeks.",
+    vendor: "Som Saa",
+    serves: "2",
+    imageUrl:
+      "https://www.dishpatch.co.uk/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0265%2F8847%2F9530%2Fproducts%2Fchicken-golae-feast-somsaa-769368.jpg%3Fv%3D1619798012&w=3840&q=75",
+    allergens: null,
+    dietaries: {
+      vegetarian: true,
+      vegan: false,
+      glutenFree: false,
+      dairyFree: true,
+    },
+  },
+  {
+    title: undefined,
+    handle: "mexican-taco-feast",
+    description:
+      "Authentic, corn tortillas with marinated, slow-braised lamb shank, pork shoulder or braised short rib.",
+    vendor: "El Pastor",
+    serves: 2,
+    imageUrl:
+      "https://www.dishpatch.co.uk/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0265%2F8847%2F9530%2Fproducts%2Ftaco-party-feast-el-pastor-558105.jpg%3Fv%3D1635342236&w=3840&q=75",
+    allergens: ["gluten", "nuts"],
+    dietaries: {
+      vegetarian: false,
+      vegan: false,
+      glutenFree: false,
+      dairyFree: false,
+    },
+  },
+];
